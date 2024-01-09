d5bd265662947313dcf6e7560925b65b9042ae7c
lakefishingman522 <calvingill89@gmail.com>
2024-01-09 15:56:47 +0900

    feat: used cyptocompare api

--- cmd/main.go
+++ cmd/main.go
@@ -1,20 +1,74 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
-	gecko "github.com/verzth/go-coingecko"
+	"github.com/joho/godotenv"
+
+	"github.com/gin-gonic/gin"
+
+	db "github.com/CascadiaFoundation/CascadiaTokenScrapper/db"
+
+	"github.com/CascadiaFoundation/CascadiaTokenScrapper/handlers"
 )
 
 func main() {
-	cgClient := gecko.NewClient(nil)
-	defer cgClient.Close()
-
-	data, err := cgClient.CoinsId("bitcoin", true, true, true, false, false, false)
+	err := godotenv.Load(".env")
 	if err != nil {
-		fmt.Print("Somethig went wrong...")
-		return
+		panic(err)
 	}
-	bitcoinPrice, _ := data.MarketData.CurrentPrice.Usd.Float64()
-	fmt.Printf("Bitcoin price is: %f$", bitcoinPrice)
+
+	// Initialize a database
+	DB, err := db.Init()
+
+	if err != nil {
+		panic(err)
+	}
+
+	h := handlers.New(DB)
+
+	// Create routers
+	router := gin.Default()
+
+	// Create a new endpoint /health
+	router.GET("/getStatistics", h.HealthCheck)
+
+	srv := &http.Server{
+		Addr:    ":3000",
+		Handler: router,
+	}
+
+	go func() {
+		// service connections
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	// Wait for interrupt signal to gracefully shutdown the server with
+	// a timeout of 5 seconds.
+	quit := make(chan os.Signal, 1)
+	// kill (no param) default send syscall.SIGTERM
+	// kill -2 is syscall.SIGINT
+	// kill -9 is syscall. SIGKILL but can't be catch, so don't need add it
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutdown Server ...")
+
+	// Create a timeout context used to gracefully shutdown the server with a timeout
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server Shutdown:", err)
+	}
+	// catching ctx.Done(). timeout of 5 seconds.
+	<-ctx.Done()
+
+	log.Println("Server exiting")
 }
